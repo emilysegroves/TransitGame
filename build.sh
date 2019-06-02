ba02fb45d3e477caf97703a28d67f982b8cb3e32
emilysegroves <40738780+emilysegroves@users.noreply.github.com>
2019-06-02 17:06:32 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+javac ./transitlines/TransitLine.java
+javac ./disasters/Disasters.java
+javac ./maps/Map.java
+javac ./gui/MainGameGui.java
+javac ./gui/Gui.java
+javac TransitGame.java
+java TransitGame
